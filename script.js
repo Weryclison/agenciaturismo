80d1923d2419f1bb7107c6c8b3f0546c1f01e4f7
Weryclison Nandi <weryclison.nandi@gmail.com>
2024-01-03 18:37:10 -0300

    add faq

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const body = document.body;
 body.classList.add("js");
 
 import menuMobile from "./js/menu-mobile.js";
+import perguntas from "./js/abrir-resposta.js";
 
 if (body.classList.contains("js")) {
   menuMobile();
+  perguntas();
 }
